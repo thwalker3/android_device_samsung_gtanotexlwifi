Valera1978/android_device_samsung_gtaxlwifi

init to gtaxlwifi

lineage.mk
@@ -15,18 +15,18 @@
 #
 
 # Inherit device configuration
-$(call inherit-product, device/samsung/gtaxllte/device.mk)
+$(call inherit-product, device/samsung/gtaxlwifi/device.mk)
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := lineage_gtaxllte
-PRODUCT_DEVICE := gtaxllte
+PRODUCT_NAME := lineage_gtaxlwifi
+PRODUCT_DEVICE := gtaxlwifi
 PRODUCT_BRAND := samsung
-PRODUCT_MODEL := SM-T585
+PRODUCT_MODEL := SM-T580
 PRODUCT_MANUFACTURER := samsung
 PRODUCT_GMS_CLIENTID_BASE := android-samsung

thwalker3/android_device_samsung_gtanotexlwifi

lineage.mk
@@ -15,18 +15,22 @@
 #
 
 # Inherit device configuration
-$(call inherit-product, device/samsung/gtaxllte/device.mk)
+$(call inherit-product, device/samsung/gtaxlwifi/device.mk)
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_tablet_wifionly.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := lineage_gtaxllte
-PRODUCT_DEVICE := gtaxllte
+PRODUCT_NAME := lineage_gtaxlwifi
+PRODUCT_DEVICE := gtaxlwifi
 PRODUCT_BRAND := samsung
-PRODUCT_MODEL := SM-T585
+PRODUCT_MODEL := SM-T580
 PRODUCT_MANUFACTURER := samsung
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_FINGERPRINT= samsung/gtaxlwifixx/gtaxlwifi:7.0/NRD90M/T580XXU2BQI9:user/release-keys \
+    PRIVATE_BUILD_DESC= "gtaxlwifixx-user 7.0 NRD90M T580XXU2BQI9 release-keys"