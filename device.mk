cf63a18850a3bf5b3ee23bc009e3cabd820dd0c0
followmsi <followmsi@gmail.com>
2018-09-22 12:50:27 +0200

    gtaxlwifi: revert GPS config

--- device.mk
+++ device.mk
@@ -230,13 +230,11 @@ PRODUCT_COPY_FILES += \
 
 # GPS
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/gps/gps.cfg:$(TARGET_COPY_OUT_VENDOR)/etc/gps.cfg \
+    $(LOCAL_PATH)/configs/gps/gps.conf:$(TARGET_COPY_OUT_VENDOR)/etc/gps.conf \
     $(LOCAL_PATH)/configs/gps/gps.xml:$(TARGET_COPY_OUT_VENDOR)/etc/gps.xml
 
 PRODUCT_PACKAGES += \
-    android.hardware.gnss@1.1-service
-
-#    android.hardware.gnss@1.0-impl
+    android.hardware.gnss@1.0-impl
 
 PRODUCT_PACKAGES += \
     gpsd_shim
